33f04e29a7e3464f46e6dfcd6b74392f034e4766
florentmeyniel <florent.meyniel@gmail.com>
2019-10-18 17:41:47 +0200

    "refactor output of IO and add new fields"

--- IdealObserver.py
+++ IdealObserver.py
@@ -13,6 +13,7 @@ make a wrapper for the case of uncoupled TP
 
 import Inference_NoChangePoint as IO_fixed
 import Inference_ChangePoint as IO_hmm
+import numpy as np
 
 def IdealObserver(seq, ObsType, order=0, Nitem=None, options=None):
     """
@@ -28,12 +29,35 @@ def IdealObserver(seq, ObsType, order=0, Nitem=None, options=None):
     if ObsType.lower() == 'fixed':
         prior = IO_fixed.symetric_prior(order=order, Nitem=Nitem, weight=1)
         Decay, Window = parse_options(options, 'fixed_type')
+        
+        # Get posterior
         count = IO_fixed.count_tuple(seq, order=order, Nitem=Nitem, Decay=Decay, Window=Window)
-        out = IO_fixed.posterior_no_jump(count=count, prior=prior, Nitem=Nitem, order=order)
+        post = IO_fixed.posterior_no_jump(count=count, prior=prior, Nitem=Nitem, order=order)
+        
+        # Fill output
+        out = {}
+        for item in post['mean'].keys():
+            out[item] = {}
+            out[item]['mean'] = post['mean'][item]
+            out[item]['MAP'] = post['MAP'][item]
+            out[item]['SD'] = post['SD'][item]
+        out['surprise'] = compute_surprise(seq, out, order)
 
     if ObsType.lower() == 'hmm':
         resol, p_c = parse_options(options, 'hmm_param')
-        out = IO_hmm.compute_inference(seq=seq, resol=resol, order=order, Nitem=Nitem, p_c=p_c)
+        
+        # Get full posterior
+        marg_post = IO_hmm.compute_inference(seq=seq, resol=resol, order=order, Nitem=Nitem, p_c=p_c)
+        
+        # Fill output
+        out = {}
+        pgrid = np.linspace(0, 1, resol)
+        for item in marg_post.keys():
+            out[item] = {}
+            out[item]['dist'] = marg_post[item]
+            out[item]['mean'] = compute_mean_of_dist(marg_post[item], pgrid)
+            out[item]['SD'] = compute_sd_of_dist(marg_post[item], pgrid, Nitem)
+        out['surprise'] = compute_surprise(seq, out, order)
 
     return out
 
@@ -71,3 +95,32 @@ def check_options(options):
     for item in options.keys():
         checked_options[item.lower()] = options[item]
     return checked_options
+
+def compute_mean_of_dist(dist, pgrid):
+    """ Compute mean of probability distribution"""
+    return dist.transpose().dot(pgrid)
+
+def compute_sd_of_dist(dist, pgrid, Nitem):
+    """ Compute SD of probability distribution"""
+    if Nitem ==2:
+        m = compute_mean_of_dist(dist, pgrid)
+        v = dist.transpose().dot(pgrid**2) - m**2;
+    else:
+        # when there are >2 items, we interpret X-E[X] as the Euclidian distance
+        # to be done!
+        v = np.nan
+        
+    return np.sqrt(v)
+
+def compute_surprise(seq, out, order):
+    """
+    Compute surprise, conditional on the specified order
+    """
+    surprise = np.nan * np.ones(len(seq))
+    if order == 0:
+        for t in range(1, len(seq)):
+            surprise[t] = -np.log2(out[(seq[t],)]['mean'][t-1])
+    else:
+        for t in range(order, len(seq)):
+            surprise[t] = -np.log2(out[tuple(seq[t-order:t+1])]['mean'][t-1])
+    return surprise
